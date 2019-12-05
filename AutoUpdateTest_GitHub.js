ea1717c5c33d3da1f8071a3dd5ff9d08c6694a24
hiro4476 <hiro4476.r@gmail.com>
2019-12-05 18:05:03 +0900

    test
    test

---
+++ AutoUpdateTest_GitHub.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         AutoUpdateTest_GitHub
+// @namespace    http://hiro.net/AutoUpdateTest_GitHub
+// @version      0.11
+// @description  try to take over the world!
+// @author       hiro
+// @match        *
+// @updateURL    
+// @downloadURL  
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("ASD1");
+    // Your code here...
+})();
