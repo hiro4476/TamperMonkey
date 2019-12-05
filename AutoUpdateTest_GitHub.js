af8891b514623b60aa41ff412a3b584d83848d58
hiro4476 <hiro4476.r@gmail.com>
2019-12-05 18:10:06 +0900

    Update AutoUpdateTest_GitHub.js

--- AutoUpdateTest_GitHub.js
+++ AutoUpdateTest_GitHub.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         AutoUpdateTest_GitHub
 // @namespace    http://hiro.net/AutoUpdateTest_GitHub
-// @version      0.11
+// @version      0.1
 // @description  try to take over the world!
 // @author       hiro
 // @match        *
-// @updateURL    
-// @downloadURL  
+// @updateURL    https://raw.githubusercontent.com/hiro4476/TamperMonkey/master/AutoUpdateTest_GitHub.js?token=AAX3AXXNW3FCICGGVOTD3EK55DEQS
+// @downloadURL  https://raw.githubusercontent.com/hiro4476/TamperMonkey/master/AutoUpdateTest_GitHub.js?token=AAX3AXXNW3FCICGGVOTD3EK55DEQS
 // @grant        none
 // ==/UserScript==
 
