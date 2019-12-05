6c89243fb2c3652e109cc424764d10732e51660c
hiro4476 <hiro4476.r@gmail.com>
2019-12-05 18:27:51 +0900

    Update AutoUpdateTest_GitHub.js

--- AutoUpdateTest_GitHub.js
+++ AutoUpdateTest_GitHub.js
@@ -1,21 +1,20 @@
 // ==UserScript==
 // @name         AutoUpdateTest_GitHub
 // @namespace    http://hiro.net/AutoUpdateTest_GitHub
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       hiro
 // @match        *
 // @updateURL    https://github.com/hiro4476/TamperMonkey/raw/master/AutoUpdateTest_GitHub.js
-// @downloadURL  https://github.com/hiro4476/TamperMonkey/raw/master/AutoUpdateTest_GitHub.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    console.log("ASD2");
-    console.log("ASD2");
-    console.log("ASD2");
-    console.log("ASD2");
-    console.log("ASD2");
+    console.log("ASD3");
+    console.log("ASD3");
+    console.log("ASD3");
+    console.log("ASD3");
+    console.log("ASD3");
     // Your code here...
 })();
